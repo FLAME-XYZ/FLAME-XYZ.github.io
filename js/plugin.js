f567d9ff40d22aa1c35d1d2c12241a31baedfd51
JO X CODE <joanadisaputra@gmail.com>
2024-06-20 01:00:10 +0700

    Add files via upload

---
+++ js/plugin.js
@@ -0,0 +1,144 @@
+const progress = document.getElementById("progress");
+const song = document.getElementById("song");
+const controlIcon = document.getElementById("controlIcon");
+const playPauseButton = document.querySelector(".play-pause-btn");
+const forwardButton = document.querySelector(".controls button.forward");
+const backwardButton = document.querySelector(".controls button.backward");
+const songName = document.querySelector(".music-player h1");
+const artistName = document.querySelector(".music-player p");
+const Button = document.querySelector(".btn");
+
+const songs = [
+  {
+    title: "Indilla",
+    name: "Love Story",
+    source:
+      "indilaslow.mp3",
+  },
+  {
+    title: "Boboiboy Galaxy",
+    name: "Dunia Baru",
+    source:
+      "bbb.mp3",
+  },
+  {
+    title: "Ultraman Nexus",
+    name: "Opening 1",
+    source:
+      "nexus.mp3",
+  },
+  {
+    title: "",
+    name: "Kamen Rider Zi-o",
+    source:
+      "exaid.mp3",
+  },
+  {
+    title: "Tokyo Revenger",
+    name: "Opening 3",
+    source:
+      "naruto.mp3",
+  },
+
+  {
+    title: "Dj Japan",
+    name: "Amend Reverb",
+    source:
+      "japan.mp3",
+  },
+  {
+    title: "A Little Piece Of Heaven",
+    name: "Avenged Sevenfold",
+    source:
+      "a little piece.mp3",
+  },
+];
+
+let currentSongIndex = 3;
+
+function updateSongInfo() {
+  songName.textContent = songs[currentSongIndex].title;
+  artistName.textContent = songs[currentSongIndex].name;
+  song.src = songs[currentSongIndex].source;
+
+  song.addEventListener("loadeddata", function () {});
+}
+
+song.addEventListener("timeupdate", function () {
+  if (!song.paused) {
+    progress.value = song.currentTime;
+  }
+});
+
+song.addEventListener("loadedmetadata", function () {
+  progress.max = song.duration;
+  progress.value = song.currentTime;
+});
+
+function pauseSong() {
+  song.pause();
+  controlIcon.classList.remove("fa-pause");
+  controlIcon.classList.add("fa-play");
+}
+
+function playSong() {
+  song.play();
+  controlIcon.classList.add("fa-pause");
+  controlIcon.classList.remove("fa-play");
+}
+
+function playPause() {
+  if (song.paused) {
+    playSong();
+  } else {
+    pauseSong();
+  }
+}
+
+playPauseButton.addEventListener("click", playPause);
+
+progress.addEventListener("input", function () {
+  song.currentTime = progress.value;
+});
+
+progress.addEventListener("change", function () {
+  playSong();
+});
+
+forwardButton.addEventListener("click", function () {
+  currentSongIndex = (currentSongIndex + 1) % songs.length;
+  updateSongInfo();
+  playPause();
+});
+
+backwardButton.addEventListener("click", function () {
+  currentSongIndex = (currentSongIndex - 1 + songs.length) % songs.length;
+  updateSongInfo();
+  playPause();
+});
+
+
+
+updateSongInfo();
+
+var swiper = new Swiper(".swiper", {
+  effect: "coverflow",
+  centeredSlides: true,
+  initialSlide: 3,
+  slidesPerView: "auto",
+  allowTouchMove: false,
+  spaceBetween: 40,
+  coverflowEffect: {
+    rotate: 25,
+    stretch: 0,
+    depth: 50,
+    modifier: 1,
+    slideShadows: false,
+  },
+  navigation: {
+    nextEl: ".forward",
+    prevEl: ".backward",
+  },
+});
+
+// Inspiration: https://dribbble.com/shots/5455156-Car-HMI-assistant-Album-switching
